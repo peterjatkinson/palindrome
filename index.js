55903fca3e0b65d832aae7aa081c9c4ad249ab95
Peter Atkinson <peterjatkinson@gmail.com>
2020-12-21 10:56:42 +0000

    Added a readme file

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
-
-// Add a blank method to the String object prototype that returns true if the string is empty or consists solely of whitespace (spaces, tabs, or newlines)
-String.prototype.blank = function () {
-  return !!(this.match(/^\s*$/g));
-}
-
 
 // Defines a Phrase object.
 function Phrase(content) {
@@ -23,29 +19,3 @@ function Phrase(content) {
     return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-
-
-// // Defines a TranslatedPhrase object.
-// function TranslatedPhrase(content, translation) {
-//   this.content = content;
-//   this.translation = translation;
-
-//   // Returns translation processed for palindrome testing.
-//   this.processedContent = function processedContent() {
-//     return this.processor(this.translation);
-//   }
-// }
-
-// TranslatedPhrase.prototype = new Phrase();
-
-
-
-// Returing last element of an array exercise. Either of these two.
-// Array.prototype.last = function() {
-// 	return this[this.length-1];
-// }
-
-// Array.prototype.lastUsingSlicing = function() {
-//   return this.slice(-1)[0];
-// }
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Phrase object (with palindrome detector)
+This is a sample NPM module created as part of
+[*Learn Enough JavaScript to Be Dangerous*](https://www.learnenough.com/javascript-tutorial). The module can be used as follows:
+
+<!-- ```bash
+$ npm install --global kurple-palindrome
+$ vim test.js
+let Phrase = require("kurple-palindrome");
+let napoleonsLament = new Phrase("Able was I, ere I saw Elba.");
+console.log(napoleonsLament.palindrome());
+$ node test.js
+true
+``` -->
